db1475664d65628bbe06fbd0d63b54fe67f9c4bf
José Franklin Silva Alves <josealves@MBP-de-Jose-Alves.local>
2021-11-03 20:32:00 -0300

    clean project

--- Example/Pods/Target Support Files/Theme/Theme-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Theme : NSObject
-@end
-@implementation PodsDummy_Theme
-@end
--- Example/Pods/Target Support Files/UNIPPlusPods/UNIPPlusPods-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_UNIPPlusPods : NSObject
-@end
-@implementation PodsDummy_UNIPPlusPods
-@end
