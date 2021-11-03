1313397f7ea5beb0ff0a0d60ff016f3ca1948ba1
José Franklin Silva Alves <josealves@MBP-de-Jose-Alves.local>
2021-11-03 00:19:42 -0300

    added theme file

---
+++ Example/Pods/Target Support Files/Theme/Theme-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Theme : NSObject
+@end
+@implementation PodsDummy_Theme
+@end
---
+++ Theme/UPTheme.swift
@@ -0,0 +1,13 @@
+//
+//  UPTheme.swift
+//  Pods
+//
+//  Created by José Franklin Silva Alves on 03/11/21.
+//
+
+import Foundation
+
+public class UPTheme {
+    
+    public init() {}
+}
